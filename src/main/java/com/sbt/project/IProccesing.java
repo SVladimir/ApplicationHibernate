98102c01b8d24330f9095ab23f41561315ed0937
vsshmalko <vsshmalko@gmail.com>
2016-11-02 22:49:30 +0300

    init

--- src/main/java/com/sbt/project/IProccesing.java
+++ src/main/java/com/sbt/project/IProccesing.java
@@ -3,4 +3,5 @@ package com.sbt.project;
  * Created by vsshm_000 on 29.10.2016.
  */
 public interface IProccesing {
+    public void procces(Document document);
 }
---
+++ src/main/java/com/sbt/project/Proccessing.java
@@ -0,0 +1,12 @@
+package com.sbt.project;
+
+/**
+ * Created by vsshm_000 on 02.11.2016.
+ */
+public class Proccessing implements IProccesing{
+
+    @Override
+    public void procces(Document document) {
+
+    }
+}
