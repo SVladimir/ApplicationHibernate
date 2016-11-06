78c233620418bd075c39b4cf85bafeab41b04aa7
ShmalkoSG <s.shmalko@yandex.ru>
2016-11-06 10:43:16 +0300

    add Test & Processing

---
+++ src/main/java/com/sbt/project/Cancel.java
@@ -0,0 +1,11 @@
+package com.sbt.project;
+
+/**
+ * Created by svetlanashmalko on 06.11.16.
+ */
+public class Cancel implements ICancel {
+    @Override
+    public void cancel(Document document) {
+
+    }
+}
---
+++ src/main/java/com/sbt/project/Processing.java
@@ -0,0 +1,11 @@
+package com.sbt.project;
+
+/**
+ * Created by svetlanashmalko on 06.11.16.
+ */
+public class Processing implements IProccesing {
+    @Override
+    public void procces(Document document) {
+
+    }
+}
