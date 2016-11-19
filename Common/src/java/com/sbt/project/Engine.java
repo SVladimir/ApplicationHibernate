91b0b540fb72c0670fca6ee7b4c07d1b9f0cd268
vsshmalko <vsshmalko@gmail.com>
2016-11-19 16:13:11 +0300

    split module

---
+++ Common/src/java/com/sbt/project/Engine.java
@@ -0,0 +1,18 @@
+package com.sbt.project;
+
+/**
+ * Created by vsshm_000 on 02.11.2016.
+ */
+
+public class Engine {
+    public static void main(String[] args) {
+        try {
+            MessageReceiver messageReceiver = new MessageReceiver();
+            System.out.printf("To processing=" + messageReceiver.recciev());
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
